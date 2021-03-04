6631d0fe5bd0ecb7ca16eaac39e19ff6f1813251
Randy Zhu <randyzhu@protonmail.com>
2021-03-04 15:44:33 -0800

    kjbhbkjbihuiuiguigiugdsgfjlgjlkgfdkljgfdkljgdfjweopwrl

--- robot/robot.py
+++ robot/robot.py
@@ -1,7 +1,6 @@
 """
 Team 8330's auto-magical auto-routine robot.
 """
-import magicbot
 import wpilib
 import wpilib.drive
 
@@ -14,15 +13,31 @@ class Robot(wpilib.TimedRobot):
         self.right_motor_front = wpilib.PWMVictorSPX(1)
         self.left_motor_front = wpilib.PWMVictorSPX(2)
         self.left_motor_back = wpilib.PWMVictorSPX(3)
+        self.arm_motor = wpilib.PWMVictorSPX(4)
+        self.arm_abc = wpilib.PWMVictorSPX(5)
 
         # Create SpeedControllers as we're using a gearbox.
         self.right_speed_ctrl_group = wpilib.SpeedControllerGroup(self.right_motor_back, self.right_motor_front)
-        self.left_speed_ctrl_group = wpilib.SpeedControllerGroup(self.left_motor_back, self.right_motor_front)
+        self.left_speed_ctrl_group = wpilib.SpeedControllerGroup(self.left_motor_back, self.left_motor_front)
 
+        # Drive class
         self.diff_drive = wpilib.drive.DifferentialDrive(self.left_speed_ctrl_group, self.right_speed_ctrl_group)
 
+        # Timer class.
+        self.timer = wpilib.Timer()
+
+    def autonomousInit(self) -> None:
+        self.timer.reset()
+        self.timer.start()
+
     def autonomousPeriodic(self) -> None:
-        self.diff_drive.curvatureDrive(0.2, 0.8, False)
+        print(self.timer.get())
+        if self.timer.get() > 5 and :
+            # self.arm_motor.set(1)
+            self.arm_abc.set(0.5)
+        else:
+            self.diff_drive.curvatureDrive(-0.2, 0, False)
+            self.arm_abc.set(-0.5)
 
 if __name__ == "__main__":
     wpilib.run(Robot)
